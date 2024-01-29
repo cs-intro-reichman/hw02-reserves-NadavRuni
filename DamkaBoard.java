ff1e7200724b315bfebb5d7f36acede8ba10a056
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-01-29 22:47:21 +0200

    1

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,26 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
-	}
-}
+		
+
+				int x=Integer.parseInt(args[0]);
+				int i=1;
+				int l=0;
+				while (i<=x)
+				{
+					if (i%2==0) System.out.print(" ");
+					while (l<x)
+					{
+						System.out.print("* ");
+						l++;
+					}
+					l=0;
+					System.out.println();
+					i++;
+					
+				}
+			}
+		
+		}
+		
+	
