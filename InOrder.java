c5928783a6e65fd68c607788861c252c1b5f8fc6
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-01-29 22:53:58 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,29 @@
-/**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
- */
+
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+	public static void main(String[] args) {
+		double rand;
+		int a, big;
+		boolean b = true;
+		rand = (Math.random() * (10));
+		a = (int) rand;
+		big = a;
+		System.out.print(a + " ");
+		while (b == true) {
+			rand = (Math.random() * (10));
+			a = (int) rand;
+			if (big > a)// בדיקה האם המספר החדש יותר קטן
+			{
+				b = false;
+				System.out.println("");
+				System.out.println("next number was " + a);// אמנם לא במסגרת המשימה , אך נמצא פה על מנת לבדוק את עצמי,
+															// תסלח לי בודק יקר
+			} else {
+				big = a;
+				System.out.print(a + " ");
+
+			}
+		}
+
 	}
+
 }
