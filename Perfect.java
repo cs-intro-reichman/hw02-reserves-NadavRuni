38afa506ebf22870c38d9ea8c2c8fdbac93183e8
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-03-27 00:53:54 +0200

    perfect1

--- Perfect.java
+++ Perfect.java
@@ -1,34 +1,33 @@
 public class Perfect {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
-		int i = 1, sum = 0;
+		int i = 2, sum = 1;
 		int remi = 0;
 		boolean b = false;
-		while ((i < x) && (b == false)) {
-			sum = sum + i;
-			if (x == sum) {
-				remi = i;
-				i = 1;
-				System.out.print(x + " is a perfect number since " + x + " = ");
-				while (i <= remi) {
-					if (i < remi) {
-						System.out.print(i + " + ");
-					} else {
-						System.out.print(i);
-					}
-
-					i++;
-
-				}
-
-				b = true;
+		int mod;
+		String end=x + " is a perfect number since 1";
+		while (i < x) {
+			mod = x % i;
+			if (mod == 0)
+			{
+				sum+=i;
+				end=end +" + "+ i ; 
+				
 			}
-
+				
 			i++;
 		}
-
-		if (b == false)
+		if (sum==x)
+		{
+			end=end +" = "+x;
+			System.out.println(end);
+		}
+		
+          
+		else 
+		{
 			System.out.println(x + " is not a perfect number ");
+		}
 
 	}
 
