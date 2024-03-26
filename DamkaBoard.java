20741493edc0e2f75780d62660f1ad2717cbd0cd
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-03-26 21:39:37 +0200

    c

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,14 +7,29 @@ public class DamkaBoard {
 		int x = Integer.parseInt(args[0]);
 		int i = 1;
 		int l = 0;
+		boolean q;
 		while (i <= x) {
-			if (i % 2 == 0)
+			q=(i % 2 == 0);
+			if (q)
+			{
 				System.out.print(" ");
+			}
+				
 			while (l < x-1) 
 			{
 				System.out.print("* ");
 				l++;
-			}System.out.print("*");
+			}
+			if (q)
+			{
+				System.out.print("* ");
+			}
+			else
+			{
+				System.out.print("*");
+
+			}
+			
 			l = 0;
 			System.out.println();
 			i++;
