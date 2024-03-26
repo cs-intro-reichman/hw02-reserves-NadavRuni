6c7d88dcbe6f50b6016d2b59cc913321e3feede6
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-03-26 21:21:51 +0200

    1

--- Reverse.java
+++ Reverse.java
@@ -6,7 +6,19 @@
 public class Reverse {
 	public static void main(String[] args) {
 
-		String x = args[0];
+		int x=3;
+		int [] y ={1,2,3};
+		
+		triple1(x);
+		triple2(y);
+		System.out.println(x);
+		for(int i =0;i<x;i++)
+			{
+				System.out.print(y[i]+" ");
+			}
+		
+	}
+		/**String x = args[0];
 		int l = x.length();
 		int i = 0, place = l - 1;
 		int middle = place / 2;
@@ -17,6 +29,16 @@ public class Reverse {
 		}
 		System.out.println("");
 		System.out.println("The middle character is " + x.charAt(middle));
+		 */
+		public static void triple1(int a){
+			a=3*a;
+		}
+		public static void triple2(int[] a){
+			for(int i =0;i<a.length;i++)
+			{
+				a[i]=3*a[i];
+			}
+		}
 
 	}
-}
+
